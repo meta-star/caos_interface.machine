79717d93ae6348ab8a3a6aa4f15404c446d47cc9
SuperSonic <supersonic@livemail.tw>
2022-06-02 16:18:07 +0800

    Update token_downloaded url

--- src/init/cloud_token.py
+++ src/init/cloud_token.py
@@ -6,7 +6,7 @@ from pathlib import Path
 CLOUD_TOKEN_FILENAME = "data/cloud_token.json"
 
 if not Path(CLOUD_TOKEN_FILENAME).exists():
-    token_downloaded = requests.get("https://caos-api.startw.cf/v1/register")
+    token_downloaded = requests.get("https://caos-api.startw.cf/v1/machine/register")
     with open(CLOUD_TOKEN_FILENAME, "w") as token_file:
         token_file.write(token_downloaded.text)
     cloud_token = token_downloaded.json()
