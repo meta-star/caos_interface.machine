e5ee485485751a709da4fcd8007b106cca92c0f8
SuperSonic <supersonic@livemail.tw>
2022-10-26 10:52:32 +0800

    Update

---
+++ app.js
@@ -0,0 +1,24 @@
+// caOS
+// (c) 2022 Star Inc.
+"use strict";
+
+// Create context storage
+const ctx = {
+    now: () => Math.floor(new Date().getTime() / 1000),
+};
+
+// Load kernel modules
+require('./src/kernel')(ctx);
+
+// Load browser modules
+require('./src/browser')(ctx);
+
+// Show status message
+console.info(
+    "index.linux - The UI System for caOS",
+    "\n====",
+    `\nStarted: ${ctx.now()}`,
+    `\nPID: ${process.pid}`,
+    `\nUnixPlatform: ${process.platform}`,
+    `\nDaemon UnixSocket: ${process.env.DAEMON_UNIX_SOCKET_PATH}`,
+);
